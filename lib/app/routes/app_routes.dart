2dfeee01a14b16bfc1d360f719e2afb27cf97823
mr-santos <mr-santos@mr-santos.mr-santos>
2024-06-26 16:13:24 -0300

    FEAT:  Add file in routes in app

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes{
+  static String root = '/';
+  static String login = '/login';
+  static String createUser = '/createUser';
+  static String home = '/home';
+
+  static String authModule = '/authModule';
+}
