589d7f1cb4726c8f4db8b1b2ead2ba70d6c8a563
mr-santos <mr-santos@mr-santos.mr-santos>
2024-06-26 16:20:31 -0300

    Add modules in app module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -17,16 +17,21 @@ import 'package:app_repense/app/modules/tecnologias/tecnologias_module.dart';*/
 import 'package:core_kb/core_kb.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'modules/auth/auth_module.dart';
+import 'modules/splash/splash_module.dart';
+import 'routes/app_routes.dart';
+
 
 class AppModule extends Module{
 
   @override
-  List<Module> get imports => [CoreModule()];//, AuthModule()];
+  List<Module> get imports => [CoreModule(), AuthModule()];
   
   @override
   void routes(r){
-    /*r.module(AppRoutes.root, module: SplashModule());
+    r.module('/', module: SplashModule());
     r.module(AppRoutes.authModule, module: AuthModule());
+    /*r.module(AppRoutes.root, module: SplashModule());
     r.module(AppRoutes.jobsModule, module: JobsModule());
     r.module(AppRoutes.curriculumModule, module: CurriculoModule());
     r.module(AppRoutes.splashModule, module: SplashModule());
--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,16 +1,25 @@
+import 'package:core_kb/core_kb.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:babigol/babigol.dart';
+import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget{
   const AppWidget({super.key});
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: lightTheme(context),
-      debugShowCheckedModeBanner: false,
-      routerConfig: Modular.routerConfig,
+      return ChangeNotifierProvider(
+      create: (context) => ThemeController(),
+      child: Consumer<ThemeController>(
+        builder: (context, controller, child) {
+          return MaterialApp.router(
+            theme: lightTheme(context),
+            debugShowCheckedModeBanner: false,
+            routerConfig: Modular.routerConfig,
+          );
+        }
+      )
     );
   }
 
