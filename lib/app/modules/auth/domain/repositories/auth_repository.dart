3b58c8f0e8dc04606b4e88620221b2d77eb6c399
mr-santos <mr-santos@mr-santos.mr-santos>
2024-06-28 11:41:00 -0300

    FEAT:  creating auth repository abstraction

---
+++ lib/app/modules/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,11 @@
+import '../entities/login.dart';
+import '../entities/token.dart';
+
+abstract class AuthRepository{
+  //Future<User> createUser(CreateUser user);
+  Future<Token> login(Login login);
+  //Future<Token> refreshToken(Refresh refresh);
+  //Future<User> myUser();
+  Future<String?> interception();
+  Future<String?> checkForUpdate();
+}
